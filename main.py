c98ffb105b27e9ceb7457e400976c15b4f9f7539
yanwhite1017 <52163207+yanwhite1017@users.noreply.github.com>
2023-02-18 19:09:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def handle_function(message):
 													symbol=name_pair,
 													qty=qValue)
 					try:
-						telegram_bot.send_message('-1001550657696', f'🟢 Short FVG {15} {name_pair}\n\nОбнаружен: {candle_value}\n sl 0.20%: {stop_loss}\ntp: 0.20%: {take_profit}')
+						telegram_bot.send_message('-1001550657696', f'🟢 Short FVG {time_frame} {name_pair}\n\nОбнаружен: {candle_value}\n sl {sl}%: {stop_loss}\n tp: {tp}%: {take_profit}')
 					except Exception as exc:
 						print(exc)
 					
@@ -177,7 +177,7 @@ def handle_function(message):
 													symbol=name_pair,
 													qty=qValue)
 					try:
-						telegram_bot.send_message('-1001550657696', f'🔴 Short FVG {5} {name_pair}\n\nОбнаружен: {candle_value}\n sl 0.20%: {stop_loss}\ntp: 0.20%: {take_profit}')
+						telegram_bot.send_message('-1001550657696', f'🔴 Short FVG {time_frame} {name_pair}\n\nОбнаружен: {candle_value}\n sl {sl}%: {stop_loss}\n tp: {tp}%: {take_profit}')
 					except Exception as exc:
 						print(exc)
 					
