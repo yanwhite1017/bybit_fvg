5ee75866b6ea5f62a186a1e434056930b8038ba2
yanwhite1017 <52163207+yanwhite1017@users.noreply.github.com>
2023-02-21 17:01:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ logging.basicConfig(filename="pybit.log", level=logging.DEBUG,
 leverage = 20
 
 # Timeframe
-time_frame = "15"
+time_frame = "5"
 
 # Stop-loss
-sl = 0.50
+sl = 0.10
 
 #Take-profit
-tp = 0.50
+tp = 0.30
 
 #Pairs
 symbols = ["ETCUSDT"]
